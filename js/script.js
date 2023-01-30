7aa5883c62053d155d61c58176ca9dee1d4db4eb
pslcorrea <pslcorrea@gmail.com>
2023-01-30 14:25:06 -0300

    Alteração do projeto novo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-const btnMobile = document.querySelector("#btn-mobile");
-
-function toggleMenu() {
- const nav = document.querySelector("#nav");
- nav.classList.toggle("active");
-}
-
-btnMobile.addEventListener("click", toggleMenu);
+const btnMenu = document.querySelector("#menu-mobile ion-icon");
+const menuMobile = document.querySelector(".menu-mobile");
+btnMenu.addEventListener("click", function () {
+ menuMobile.classList.toggle("menu-mobile-open");
+});
+// menuBar.addEventListener("click", (e) => {
+//  e.preventDefault();
+//  let menuMobile = document.querySelector(".menu-mobile ul");
+//  if (menuMobile.classList.contains("show-menu")) {
+//   menuMobile.classList.remove("show-menu");
+//  } else {
+//   menuMobile.classList.add("show-menu");
+//  }
+// });
